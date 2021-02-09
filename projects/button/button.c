bd31ff1bc2679186b3e456abb413c0464983cfe5
Lin Zhong <lzhong@wimpy.local>
2021-02-08 21:37:31 -0500

    First working version

--- projects/button/button.c
+++ projects/button/button.c
@@ -3,40 +3,57 @@
  *
  * author: Furkan Cayci & Lin Zhong
  * description:
- *  green, orange and red light up sequentially 
- *  after reset or PC7 is touched
+ *   blinks LEDs one at a time using timer interrupt
+ *   timer2 is used as the source, and it is setup
+ *   to run at 10 kHz. LED blinking rate is set to
+ *   1 second.
  *
- * 
+ * timer and timer interrupt setup steps:
+ *   1. Enable TIMx clock from RCC
+ *   2. Set prescaler for the timer from PSC
+ *   3. Set auto-reload value from ARR
+ *   4. (optional) Enable update interrupt from DIER bit 0
+ *   5. (optional) Enable TIMx interrupt from NVIC
+ *   6. Enable TIMx module from CR1 bit 0
  */
 
 #include "stm32f4xx.h"
 #include "system_stm32f4xx.h"
 
 enum GPIO_pin_state {OUTPUT_HIGH, INPUT_X, INPUT_LOW};
-enum bool {true,false};
 
-/* Global indicator whether a touch is detected */
-enum bool touched = false;
+/* Global indicator whether a capicitance measurement is ready */
+int measured = 0;
 
 /*************************************************
 * function declarations
 *************************************************/
 int main(void);
+void start_input(void);
 
 /*************************************************
 * external interrupt handler
 *************************************************/
-void EXTI9_5_IRQHandler(void)
+void EXTI0_IRQHandler(void)
 {
     
     // Check if the interrupt came from exti0
-    if (EXTI->PR & (1 << 7)){
+    if (EXTI->PR & (1 << 0)){
 
-        touched = true; //Indicate measurement is ready
+        /* Stop counter */
+
+        measured = 1; //Indicate measurement is ready
         GPIOD->ODR |= (uint16_t)(0x8000); //blue on
+
+        /* wait little bit */
+        for(int j=0; j<1000000; j++);
+
         
         // Clear pending bit
-        EXTI->PR = (1 << 7);
+        EXTI->PR = (1 << 0);
+
+
+
     }
 }
 
@@ -54,7 +71,10 @@ void TIM2_IRQHandler(void)
             TIM2->SR &= ~(1U << 0);
         }
     }
-      
+    
+    /* Start Measurement */
+    
+    
     switch(state) {
 
         case INPUT_LOW:
@@ -70,21 +90,15 @@ void TIM2_IRQHandler(void)
 
             break;
         case OUTPUT_HIGH:
-            //set the PC7 as input;
-            // Set the pin 7 as input, corresponding to 15:14=00
-            //RM0090 8.4.1
-            GPIOC->MODER &= 0xFFFF3FFF; //set bit 15 to 0
-
-            touched = false; // measurement not ready
-            GPIOD->ODR &= (uint16_t)0x7FFF;//blue off
+            start_input(); 
 
             GPIOD->ODR |= 0x2000;//orange
             state = INPUT_X;
             break;
         case INPUT_X:
 
-            if(touched==true) {
-                GPIOD->ODR = 0x0000;
+            if(measured==1) {
+                GPIOD->ODR = 0x00000000;
                 state = INPUT_LOW;
             } else{
                 GPIOD->ODR |= 0x4000;//red
@@ -93,6 +107,20 @@ void TIM2_IRQHandler(void)
             break;
     } 
     
+
+}
+
+/******************************************
+ * Set PC.7 as input and interruptible
+ ******************************************/
+
+void start_input(void)
+{
+    
+    measured = 0; // measurement not ready
+    GPIOD->ODR &= (uint16_t)0x7FFF;//blue off
+
+    //Start timer counter
 
 }
 
@@ -129,7 +157,7 @@ int main(void)
     // Set the auto-reload value to 10000
     //   which should give 1 second timer interrupts
     // 10000 measn 1000 ms timer interrupt
-    TIM2->ARR = 4000;
+    TIM2->ARR = 10000;
 
     // Update Interrupt Enable
     TIM2->DIER |= (1 << 0);
@@ -140,37 +168,37 @@ int main(void)
 
     // Enable Timer 2 module (CEN, bit0)
     TIM2->CR1 |= (1 << 0);
-;
-
-    /* Configure the GPIO C.7 for touch sensing */
-    // RM0090, 8.4.4
-    // PUPDR [15:14] corresponds to Pin 7; 0b10 as pull down.
-    GPIOC->PUPDR |= 0x00008000;// set bit 15 to 1 for pull down
 
 
-    /* Tie PC.7 to EXTI7 */
-    // EXTI7 can be configured for each GPIO module.
-    //   EXTICR2: 0b 0010 XXXX XXXX XXXX
-    //               pin7 pin6 pin5 pin4
-    //
-   
+
+    
+    measured = 0;
+
+
     // enable SYSCFG clock (APB2ENR: bit 14)
     RCC->APB2ENR |= (1 << 14);
 
-    //   Writing a 0b0010 to pin7 location ties PC.70 to EXT0
-    SYSCFG->EXTICR[1] = 0x2000;// Write 0010 to map PC7 to EXTI0
+    /* tie push button at PA0 to EXTI0 */
+    // EXTI0 can be configured for each GPIO module.
+    //   EXTICR1: 0b XXXX XXXX XXXX 0000
+    //               pin3 pin2 pin1 pin0
+    //
+    //   Writing a 0b0000 to pin0 location ties PA0 to EXT0
+    SYSCFG->EXTICR[0] |= 0x00000000; // Write 0000 to map PA0 to EXTI0
 
     // Choose either rising edge trigger (RTSR) or falling edge trigger (FTSR)
-    EXTI->FTSR |= 0x00080;
+    EXTI->RTSR |= 0x00001;   // Enable rising edge trigger on EXTI0
 
-    // Unmask the corresponding interrupt line (line 7).
-    EXTI->IMR |= 0x00080;   
+    // Mask the used external interrupt numbers.
+    EXTI->IMR |= 0x00001;    // Mask EXTI0
 
     // Set Priority for each interrupt request
-    NVIC_SetPriority(EXTI9_5_IRQn, 1); // Priority level 1
+    NVIC_SetPriority(EXTI0_IRQn, 1); // Priority level 1
 
     // enable EXT0 IRQ from NVIC
-    NVIC_EnableIRQ(EXTI9_5_IRQn);
+    NVIC_EnableIRQ(EXTI0_IRQn);
+
+
 
 
 
